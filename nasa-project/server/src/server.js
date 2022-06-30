b3a9274d2d6f0f2ed713d8c4e5920dd499a48d75
Bruno Henrique <brunohbm23@gmail.com>
2022-06-29 21:47:04 -0300

    Initiating mongoDB on nasa project and testing the cluster process with many threads

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,13 +1,24 @@
 require('./config');
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+const MONGO_CONNECTION_URI = `mongodb+srv://nasa-api:${process.env.MONGO_DB_PASSWORD}@nasaapi.mnhykjz.mongodb.net/?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', err => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_CONNECTION_URI);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
